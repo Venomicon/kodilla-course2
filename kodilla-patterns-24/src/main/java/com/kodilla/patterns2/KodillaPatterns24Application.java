cc2bd182693367177553ac0024e664af2952b6a3
Venomicon <jangajda9@gmail.com>
2019-10-22 23:51:21 +0200

    Modul 24.3

--- kodilla-patterns-24/src/main/java/com/kodilla/patterns2/KodillaPatterns24Application.java
+++ kodilla-patterns-24/src/main/java/com/kodilla/patterns2/KodillaPatterns24Application.java
@@ -2,8 +2,9 @@ package com.kodilla.patterns2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class KodillaPatterns24Application {
 
     public static void main(String[] args) {
